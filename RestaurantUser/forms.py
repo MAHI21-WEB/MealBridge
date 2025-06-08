8d6f7096e404f3fd16240a57b16a941e560d4fbd
HARSHJYOT_KAUR <68178422+MAHI21-WEB@users.noreply.github.com>
2025-06-08 19:03:46 +0530

    Project Initiated

---
+++ RestaurantUser/forms.py
@@ -0,0 +1,32 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+class RestaurantUserCreationForm(UserCreationForm):
+    email= forms.EmailField(required=True)
+    city= forms.CharField(max_length=100, required=True)
+    pincode= forms.CharField(max_length=10, required=True)
+    location_link= forms.URLField(required=True)
+    fssai_certification= forms.FileField(required=True)
+    phoneno= forms.CharField(max_length=15, required=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'password1', 'password2']
+    
+    def save(self, commit=True):
+        user=super().save(commit=False)
+        user.emmail=self.cleaned_data['email']
+        if commit:
+            user.save()
+            from .models import RestaurantUser
+            RestaurantUser.objects.create(
+                user=user,
+                city=self.cleaned_data['city'],
+                pincode=self.cleaned_data['pincode'],
+                location_link=self.cleaned_data['location_link'],
+                fssai_certification=self.cleaned_data['fssai_certification'],
+                phoneno=self.cleaned_data['phoneno']
+            )
+
+        return user
---
+++ RestaurantUser/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class RestaurantUser(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    city = models.CharField(max_length=100)
+    pincode = models.CharField(max_length=10)
+    location_link = models.URLField()
+    fssai_certification = models.FileField(upload_to='certifications/restaurant/')
+    phoneno = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.user.username
